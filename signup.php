b7c5fbd4962f72bf70f0ddcf9bd779a08e400441
ehh0003 <ehh0003@mix.wvu.edu>
2021-02-16 14:35:49 -0500

    Lab 4

--- signup.php
+++ signup.php
@@ -54,7 +54,7 @@ require "includes/header.php"
                         </div>
                         -->
 
-                        <button class="btn btn-lg btn-outline-danger btn-block" name="signup-submit" type="submit">Sign in</button>
+                        <button class="btn btn-lg btn-outline-danger btn-block" name="signup-submit" type="submit">Register</button>
                         <p class="mt-5 mb-3 text-muted">&copy; 2020 4eva</p>
                     </form>
 
